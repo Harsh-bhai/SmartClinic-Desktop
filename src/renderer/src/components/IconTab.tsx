c51b58b5e93e55f239c7f6dec0346ca68a9f233a
Harsh Dewangan <harshdewangan2019@gmail.com>
2025-11-26 21:44:31 +0530

    minor name chagnes

--- src/renderer/src/components/IconTab.tsx
+++ src/renderer/src/components/IconTab.tsx
@@ -21,7 +21,7 @@ export default function IconTab() {
             size={16}
             aria-hidden="true"
           />
-          Form Settings
+          General
         </TabsTrigger>
         <TabsTrigger
           value="tab-2"
@@ -32,23 +32,23 @@ export default function IconTab() {
             size={16}
             aria-hidden="true"
           />
-          Projects
+          Form
         </TabsTrigger>
         <TabsTrigger
           value="tab-3"
           className="relative flex-col rounded-none px-4 py-2 text-xs after:absolute after:inset-x-0 after:bottom-0 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none data-[state=active]:after:bg-primary"
         >
           <BoxIcon className="mb-1.5 opacity-60" size={16} aria-hidden="true" />
-          Packages
+          Appearance
         </TabsTrigger>
       </TabsList>
       <TabsContent value="tab-1">
-        <DoctorSettingsPage />
-      </TabsContent>
-      <TabsContent value="tab-2">
         <p className="p-4 text-center text-xs text-muted-foreground">
           Content for Tab 2
         </p>
+      </TabsContent>
+      <TabsContent value="tab-2">
+        <DoctorSettingsPage />
       </TabsContent>
       <TabsContent value="tab-3">
         <p className="p-4 text-center text-xs text-muted-foreground">
